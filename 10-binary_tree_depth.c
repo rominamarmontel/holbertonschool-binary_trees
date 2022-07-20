ae0224d7c028190301327f3d78086f0c0df69dfe
rominamarmontel <romi.varnier@gmail.com>
2022-07-20 11:12:09 +0200

    betty check

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -18,6 +18,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	return (i);
 }
 /**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: a pointer to the node to measure the depth
+ * Return: 0 if tree is NULL
+ *
  * Function that measures the depth of a node
  *              in a binary tree with a recursive way
  * size_t binary_tree_depth(const binary_tree_t *tree)

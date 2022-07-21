af8367cc2d46b6b5ca4960cf51901ae8db8e6cfc
rominamarmontel <romi.varnier@gmail.com>
2022-07-21 19:11:38 +0200

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -24,9 +24,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
+
 	if (tree->left)
 		left_height = binary_tree_height(tree->left) + 1;
 	if (tree->right)

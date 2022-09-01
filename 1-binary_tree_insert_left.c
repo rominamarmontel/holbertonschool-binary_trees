5b5aeb1f276c506e7426340b1386e177e295eeee
rominamarmontel <romi.varnier@gmail.com>
2022-09-01 16:23:11 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		new->left = parent->left;
-		parent->left = new;
 		new->left->parent = new;
 	}
 	else
